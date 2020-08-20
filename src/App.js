1d27d9b62cbfc826d6b8a12a17f4e1184f4b6a13
Josh Harrington <josh@joshharrington.co.uk>
2020-08-20 12:45:41 +0100

    Setup react project Add tailwind Pull data from PunkAPI Sort and search data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+
+function getDataFromAPI({updateBeers}){
+  fetch('https://api.punkapi.com/v2/beers')
+    .then(response => response.json())
+    .then(data => {
+      const sortedByABV = data.sort((a,b) => a.abv - b.abv)
+      console.log(data, sortedByABV)
+      updateBeers(sortedByABV)
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    })
+}
 
 function App() {
+  const [beers, updateBeers] = useState(null)
+  const [searchQuery, updateSearchQuery] = useState('')
+  const [searchResults, updateSearchResults] = useState([])
+
+  useEffect(() => {
+    getDataFromAPI({updateBeers})
+  }, [])
+
+  useEffect(() => {
+    if (beers !== null) {
+
+      const beerSearch = beers.filter(beer => (
+        beer.name.toLowerCase().includes(searchQuery.toLowerCase())
+      ))
+      updateSearchResults(beerSearch)
+    }
+  }, [searchQuery])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <input
+        type="search" defaultValue={searchQuery}
+        placeholder="Search within in beer name, tagline and description"
+        onChange={(e) => updateSearchQuery(e.target.value)}
+      />
+      {!beers && <p>Loading...</p>}
+      {beers && searchQuery === '' &&
+        <ul>
+          {beers.map(b => (
+            <li key={b.id}>[{b.abv}%] {b.name} - {b.tagline}</li>
+            ))}
+        </ul>
+      }
+      {beers && searchQuery !== '' &&
+        <>
+          <h2>Search Results</h2>
+          {searchResults.length > 0 && searchResults.map(b => (
+            <li key={b.id}>[{b.abv}%] {b.name} - {b.tagline}</li>
+          ))}
+          {searchResults.length === 0 && <p>No results found</p>}
+        </>
+      }
+    </main>
   );
 }
 
